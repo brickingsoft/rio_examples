5d45afac3cc175544b565809a63473f9d55864cd
wangminxiang <ryougi.nevermore@hotmail.com>
2025-03-04 13:28:25 +0800

    mod

--- tcp_srv/main.go
+++ tcp_srv/main.go
@@ -13,6 +13,7 @@ func main() {
 	flag.IntVar(&port, "port", 9000, "server port")
 	flag.Parse()
 
+	rio.UseCPUAffinity(true)
 	ln, lnErr := rio.Listen("tcp", fmt.Sprintf(":%d", port))
 	if lnErr != nil {
 		log.Fatal("lnErr:", lnErr)
@@ -24,15 +25,18 @@ func main() {
 			return
 		}
 		go func(conn net.Conn) {
-			b := make([]byte, 1024)
-			rn, rErr := conn.Read(b)
-			if rErr != nil {
-				conn.Close()
-				return
-			}
-			_, wEr := conn.Write(b[:rn])
-			if wEr != nil {
-				conn.Close()
+			var packet [0xFFF]byte
+			for {
+				rn, rErr := conn.Read(packet[:])
+				if rErr != nil {
+					conn.Close()
+					return
+				}
+				_, wEr := conn.Write(packet[:rn])
+				if wEr != nil {
+					conn.Close()
+					return
+				}
 			}
 		}(conn)
 	}
